fdd9e88c86cb711ca3c33da72ce7b97f5848e7ec
Wassim Mbarek <mbarekwassim42@gmail.com>
2025-04-17 13:54:34 +0100

    Creation of Login Page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,8 +3,44 @@ import { useAppSelector } from '@src/modules/shared/store'
 import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import{ supabase } from '../../src/modules/shared/utils/supabase'
+import { useState, useEffect } from 'react'
 
 const App = () => {
+  /* const [user, setUser] = useState({});
+  useEffect(() => {
+    getCurrentUser();
+    window.addEventListener('hashchange', function() {
+      getCurrentUser();
+    });
+  }, [])
+  async function getCurrentUser() {
+    const user = await supabase.auth.getUser();
+    
+    setUser(user);
+  }
+  async function signInWithGithub() {
+    await supabase.auth.signInWithOAuth({
+      provider: 'github',
+      options: {
+        redirectTo: `http://localhost:3000/`,
+      },
+    })
+  }
+  async function signOut() {
+    await supabase.auth.signOut();
+    setUser({});
+  }
+  if (user) {
+    console.log('user', user);
+    return (
+      <div className="App">
+        <h1>Hello, user?</h1>
+        <button onClick={signOut}>Sign out</button>
+      </div>
+    )
+  } */
+
   const queryClient = new QueryClient()
   const { i18n } = useTranslation('translation')
   document.body.dir = i18n?.dir()
