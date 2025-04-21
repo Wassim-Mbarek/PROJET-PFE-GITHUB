ccb7e591faf2e43ab166da7ca21654e285cddb70
Wassim Mbarek <mbarekwassim42@gmail.com>
2025-04-21 22:47:48 +0100

    Update utils/common file : formatting

--- src/modules/shared/utils/common.ts
+++ src/modules/shared/utils/common.ts
@@ -1,11 +1,10 @@
-import { supabase } from "./supabase"
+import { supabase } from './supabase'
 
 export async function getConnectedUser() {
-    const { data, error } = await supabase.auth.getUser()
-    if (error) {
-      console.error('Error fetching user:', error)
-      return null
-    }
-    return data?.user?.user_metadata || null
+  const { data, error } = await supabase.auth.getUser()
+  if (error) {
+    console.error('Error fetching user:', error)
+    return null
   }
-  
+  return data?.user?.user_metadata || null
+}
