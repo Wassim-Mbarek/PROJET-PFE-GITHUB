7ed27d96bd2b614ece78c605bebb2ff4997cbd61
Wassim Mbarek <mbarekwassim42@gmail.com>
2025-06-17 15:11:13 +0100

    Update the context file

--- src/modules/shared/api/context.ts
+++ src/modules/shared/api/context.ts
@@ -1,4 +1,4 @@
-export const BootApiKey =
-  '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'
+/* export const BootApiKey =
+  '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' */
 //export const BootApiKey = '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'
 export const BootApiRoute = 'https://api.openai.com/v1/chat/completions'
